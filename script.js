1837f5107d3723ab74e2573fe21175ceb786d169
FraBif <francescobif@hotmail.it>
2023-05-14 22:02:22 +0200

    should work now

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 const userFare = parseInt(prompt(`Inserisci i km da percorrere`))
 const userAge = parseInt (prompt(`Inserisci la tua età`))
-const userFee = parseInt(userFare * 0,21)
+const userFee = parseInt(userFare * 0.21)
 const minorDiscount = parseInt((userFee * 20) / 100)
 const majorDiscount = parseInt((userFee * 40) / 100)
 
 let result = ""
 
 if (userAge < 18) {
-    result = parseInt((userFee - minorDiscount))
-    document.getElementById(`price`).innerHTML = result ;
+    result = (userFee - minorDiscount)
+    document.getElementById(`price`).innerHTML = "il prezzo da pagare è" + " " + result ;
 
 } else if (userAge > 65) {
-    result = parseInt((userFee - majorDiscount))
-    document.getElementById(`price`).innerHTML = result ;
+    result = (userFee - majorDiscount)
+    document.getElementById(`price`).innerHTML = "il prezzo da pagare è" + " " + result;
 
 } else {
     document.getElementById(`price`).innerHTML = userFee
 }
+
+
